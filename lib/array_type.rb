4fb4c8f78c3b6d0df560ab3abe469537778e5830
Donncha Redmond <dredmond@e-xact.com>
2016-06-30 12:44:10 +1000

    Don't try to cast nils

--- lib/array_type.rb
+++ lib/array_type.rb
@@ -1,8 +1,8 @@
 class ArrayType < ActiveRecord::Type::Value
 
   def type_cast(value)
-    return value if value.is_a?(Array)
-    return value.split(",")
+    return value if value.nil? || value.is_a?(Array)
+    return value.to_s.split(",")
   end
 
 end
--- lib/hash_type.rb
+++ lib/hash_type.rb
@@ -1,7 +1,7 @@
 class HashType < ActiveRecord::Type::Value
 
   def type_cast(value)
-    return value if value.is_a?(Hash)
+    return value if value.nil? || value.is_a?(Hash)
     raise NotImplementedError.new("Can't type_cast to Hash from #{value.class}")
   end
 
